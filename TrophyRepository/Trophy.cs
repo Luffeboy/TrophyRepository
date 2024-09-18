b10c83ab77e9669146ae5fc5991bb11565f11294
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2024-09-18 10:09:14 +0200

    Add project files.

---
+++ TrophyRepository/Trophy.cs
@@ -0,0 +1,28 @@
+﻿namespace TrophyRepository
+{
+    public class Trophy
+    {
+        public int Id { get; set; }
+        public string Competition { get; set; } = "";
+        public int Year { get; set; }
+
+        public override string ToString()
+        {
+            return $"This is a trophy for {Competition} from {Year}. id is {Id}";
+        }
+
+        public void ValidateCompetition()
+        {
+            if (Competition == null)
+                throw new ArgumentNullException("Competition is null " +  Competition);
+            if (Competition == "")
+                throw new ArgumentException("Competition is empty " +  Competition);
+        }
+        public void ValidateYear()
+        {
+            if (Year < 1970 || Year > 2024)
+                throw new ArgumentException("Competition is null or empty " +  Competition);
+        }
+
+    }
+}
---
+++ TrophyRepositoryTests/TrophyTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrophyRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrophyRepository.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Trophy trophy = new Trophy()
+            {
+                Id = 1,
+                Competition = "Olympic games",
+                Year = 2024
+            };
+            
+            Assert.AreEqual("This is a trophy for Olympic games from 2024. id is 1", trophy.ToString());
+        }
+
+        [TestMethod()]
+        public void ValidateCompetitionTest()
+        {
+            Trophy trophy = new Trophy()
+            {
+                Id = 1,
+                Competition = "Olympic games",
+                Year = 2024
+            };
+            trophy.ValidateCompetition();
+            trophy.Competition = null;
+            Assert.ThrowsException<ArgumentNullException>(() => trophy.ValidateCompetition());
+            trophy.Competition = "";
+            Assert.ThrowsException<ArgumentException>(() => trophy.ValidateCompetition());
+        }
+
+        [TestMethod()]
+        public void ValidateYearTest()
+        {
+            Trophy trophy = new Trophy()
+            {
+                Id = 1,
+                Competition = "Olympic games",
+                Year = 2024
+            };
+            trophy.ValidateYear();
+            trophy.Year = 1970;
+            trophy.ValidateYear();
+
+            trophy.Year = 2025;
+            Assert.ThrowsException<ArgumentException>(() => trophy.ValidateYear());
+            trophy.Year = 1969;
+            Assert.ThrowsException<ArgumentException>(() => trophy.ValidateYear());
+        }
+    }
+}
